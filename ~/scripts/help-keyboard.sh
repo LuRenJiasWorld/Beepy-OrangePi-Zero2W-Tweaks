45031f00073fed5e0bb92a09e9d12163e42018be
Benjamin Chris <loli@lurenjia.in>
2023-11-25 22:12:33 +0800

    Add help-keyboard.sh

---
+++ ~/scripts/help-keyboard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat << 'EOF'
+                 Keyboard Sym Map Help
+    Q~ | W` | E{ | R} | T[ | Y] | U< | I> | O^ | P%
+    A= | S÷ | D± | F· | G\ | H| | J& | K" | L" 
+         Z¥ | X€ | C£ | V¿ | B¡ | N« | M»
+        
+                    Meta Mode Help
+    E: up, S: down, W: left, D: right
+    R: Home, F: End, O: PageUP, P: PageDown
+    Q: Word back, A: Word forward
+    T: Tab, X: Ctrl+, C: Alt+, 0: Screen B/W,
+    N: KBLight-. M: KBLight+, $: KBLightTrigger
+EOF
